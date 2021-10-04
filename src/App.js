f0ded7ea52787380c630b7fa5d2a543d36558d85
Bruno Davanzo <chetsdavanzo@gmail.com>
2021-10-03 22:49:48 -0300

    renderizando as imagens

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
-import './App.css';
 import React from 'react';
+import { useState } from 'react';
+import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Basket from './components/Basket';
-import Main from './components/Main';
-import { useState } from 'react';
-import Product from './products.json';
-
+import MainContent from './components/Main';
 
 function App() {
-	const { products } = Product;
 	const [cartItems, setCartItems] = useState([]);
 	const onAdd = (product) => {
 		const exist = cartItems.find((x) => x.id === product.id);
 		if (exist) {
-			setCartItems(cartItems.map((x) => x.id === product.id ? { ...exist, qty: exist.qty + 1 } : x
-			)
+			setCartItems(cartItems.map((x) => 
+				x.id === product.id ? { ...exist, qty: exist.qty + 1 } : x )
 			);
 		} else {
 			setCartItems([...cartItems, { ...product, qty: 1 }]);
@@ -23,7 +20,7 @@ function App() {
 	return (
 		<div className="page">
 			<div className="catalog">
-				<Main products={products}></Main>
+				<MainContent/>
 			</div>
 			<div className="cart">
 				<Basket onAdd={onAdd} cartItems={cartItems}></Basket>
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,42 @@
-import React from 'react';
-import Item from './Item';
+// import React from 'react';
+// import Item from './Item';
 
-export default function Main(props) {
-    const { products } = props;
+// export default function Main(props) {
+//     const { products } = props;
+//     return(
+//         <main className="block col-2">
+//             <h2>Products</h2>
+//             <div className="row">
+//                 {products.map((product, index) => (
+//                 <Item key={product.id} product={product}></Item>
+//                 ))}
+//             </div>
+//         </main>
+//     )
+// }
+import React from 'react';
+import Product from '../products.json';
+import { Card, Button } from 'react-bootstrap';
+
+const MainContent = (props) => {
+    const { product, onAdd } = props;
+    const listItems = Product.map((item)=>
+    <div className="card" key={item.id}>
+        <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={item.image} />
+                <Card.Body>
+                    <Card.Title>{item.name}</Card.Title>
+                    <Card.Text>Price: R${item.price}</Card.Text>
+                    <Card.Text>Score: {item.score}</Card.Text>
+                    <Button  onClick={() => onAdd(product)} variant="primary">Add to Cart</Button>
+                </Card.Body>
+            </Card>
+    </div> 
+    )
     return(
-        <main className="block col-2">
-            <h2>Products</h2>
-            <div className="row">
-                {products.map((product) => (
-                <Item key={product.id} product={product}></Item>
-                ))}
-            </div>
-        </main>
+        <div className="catalog">
+            {listItems}
+        </div>
     )
 }
+export default MainContent;
